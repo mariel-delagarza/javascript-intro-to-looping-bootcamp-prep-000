52dea0f95f9eeac4240473dc772b76acbc87e6fd
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-14 14:50:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,10 +19,9 @@ function whileLoop(n) {
 function doWhileLoop (array) {
  var i = 0 
  function incrementVariable() {
-   i = i + 1;
+   i = i + 1;}
    do {
      array = array.pop}
   while (array.lenghth > 0 && incrementVariable())
- }
- return array;
+  return array;
 } 
